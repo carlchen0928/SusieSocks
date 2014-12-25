fb1dde93cdeebc01996f53e007fd48ff86b4c73c
cyy5121212 <515822895@qq.com>
2014-12-25 22:53:06 +0800

    main loop of eventloop

--- EventLoop.py
+++ EventLoop.py
@@ -1,24 +1,30 @@
 import socket
 import Poller
-
-'''
-fuck
-'''
+import Channel
 
 
 class EventLoop:
 	def __init__(self):
 		self._looping = False
+		self._quit = False
 		self._poller = Poller()
+		self._activeChannels = []
+		self._currentChannel = None
 		pass
 
 	def loop(self):
 		pass
 
 	def quit(self):
-		"""
+		self._quit = True
+		pass
 
+	def update_channel(self, channel):
+		pass
 
-		"""
+	def remove_channel(self, channel):
+		pass
+
+	def has_channel(self, channel):
 		pass
 
--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-import select
 
-# print select.POLLNULL
-print select.POLLIN
-print select.POLLOUT
-print select.POLLERR
-print select.POLLHUP
-print select.POLLNVAL
+class my:
+	def __init__(self):
+		self.h = 'haha'
+
+	def set(self):
+		self.h = 'xixi'
+
+m = my()
+print m.h
+
+def func(arg):
+	arg.set()
+	print arg.h
+
+func(m)
+print m.h
